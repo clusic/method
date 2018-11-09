12aefe3b842f45067228b6b4b8013726e33a44f8
evio <evio@vip.qq.com>
2018-11-09 14:09:29 +0800

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ exports.RenderMiddlewareArguments = function RenderMiddlewareArguments(object, m
   return middle;
 }
 
-exports.Controller = function Controller(prefix = '/') {
+exports.Controller = function Controller(prefix) {
   return (target, propertyKey, descriptor) => {
     if (!propertyKey && !descriptor) {
       if (global.CLUSIC_ROUTER_COMPONENTS.indexOf(target) === -1) {
